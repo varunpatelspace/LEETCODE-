d7ca3097ae118e0adaa7906cf93137aa7e3f6eaa
Varun Patel <varunspace541@gmail.com>
2026-09-03 20:01:33 +0530

    Solved roman-to-integer on LeetCode [C++]

---
+++ 2026-09/roman-to-integer.cpp
@@ -0,0 +1,40 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : roman-to-integer                                            ║
+ ║  Platform : LeetCode                                                    ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : September 3, 2026                                           ║
+ ║  URL      : https://leetcode.com/problems/divide-two-integers/description/?envType=problem-list-v2&envId=math║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+class Solution {
+public:
+    int romanToInt(string s) {
+
+        unordered_map<char, int> values = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (i + 1 < s.length() && values[s[i]] < values[s[i + 1]]) {
+                
+                ans -= values[s[i]];
+            }
+            else {
+                ans += values[s[i]];
+            }
+        }
+
+        return ans;
+    }
+};
