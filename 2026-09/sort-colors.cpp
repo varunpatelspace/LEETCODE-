54121dd53be85ca477411bf8d6ad721cb60b1232
Varun Patel <varunspace541@gmail.com>
2026-09-21 19:35:51 +0530

    Solved sort-colors on LeetCode [C++]

---
+++ 2026-09/sort-colors.cpp
@@ -0,0 +1,26 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : sort-colors                                                 ║
+ ║  Platform : LeetCode                                                    ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : September 21, 2026                                          ║
+ ║  URL      : https://leetcode.com/problems/sort-colors/?envType=problem-list-v2&envId=array║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+    for(int i=0; i < n-1; i++){
+        for(int j=0; j < n-i-1; j++ )
+        if(nums[j]>nums[j+1]){
+            swap(nums[j],nums[j+1]);
+        }
+    }
+   
+    
+    return;
+    }
+
+};
