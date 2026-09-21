ec0921c4a8f9dd48f90b3bf3795a7cda06042f66
Varun Patel <varunspace541@gmail.com>
2026-09-21 19:18:19 +0530

    Solved 3sum on LeetCode [C++]

--- 2026-09/3sum.cpp
+++ 2026-09/3sum.cpp
@@ -3,30 +3,55 @@
  ║  Problem  : 3sum                                                        ║
  ║  Platform : LeetCode                                                    ║
  ║  Status   : Accepted                                                    ║
- ║  Date     : September 16, 2026                                          ║
- ║  URL      : https://leetcode.com/problems/3sum/submissions/2143919331/?envType=problem-list-v2&envId=array║
+ ║  Date     : September 21, 2026                                          ║
+ ║  URL      : https://leetcode.com/problems/3sum/submissions/2148691121/?envType=problem-list-v2&envId=array║
  ╚═══════════════════════════════════════════════════════════════════════╝
  */
 
 class Solution {
 public:
- vector<vector<int>> threeSum(vector<int>& nums) {
- sort(nums.begin(),nums.end());
- vector<vector<int>> ans;
- for(int i=0;i<nums.size()-2;i++){
- if( i && nums[i]==nums[i-1]) 
- continue;
-  int l=i+1,r=nums.size()-1;
-  while(l<r){
- long long s=(long long)nums[i]+nums[l]+nums[r];
- if(s==0){
- ans.push_back({nums[i],nums[l],nums[r]});
- while(l<r&&nums[l]==nums[l+1])l++;
- while(l<r&&nums[r]==nums[r-1])r--;
- l++;r--;
- } else if(s<0) l++; else r--;
- }
- }
- return ans;
- }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        vector<vector<int>> res;
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 2; i++) {
+
+     
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int l = i + 1;
+            int r = n - 1;
+
+            while (l < r) {
+
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (sum == 0) {
+
+                    res.push_back({nums[i], nums[l], nums[r]});
+
+                    while (l < r && nums[l] == nums[l + 1])
+                        l++;
+
+                    while (l < r && nums[r] == nums[r - 1])
+                        r--;
+
+                    l++;
+                    r--;
+                }
+                else if (sum < 0) {
+                    l++;
+                }
+                else {
+                    r--;
+                }
+            }
+        }
+
+        return res;
+    }
 };
